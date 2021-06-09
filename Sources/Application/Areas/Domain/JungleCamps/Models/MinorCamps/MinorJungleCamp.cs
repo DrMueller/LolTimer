cf85040b8b40c5f9d21117d9d8ecaba5d7ecde3e
Matthias Müller <matthiasm@live.de>
2021-06-09 19:36:51 +0200

    WIP

--- Sources/Application/Areas/Domain/JungleCamps/Models/MinorCamps/MinorJungleCamp.cs
+++ Sources/Application/Areas/Domain/JungleCamps/Models/MinorCamps/MinorJungleCamp.cs
@@ -4,7 +4,7 @@ namespace Mmu.LolTimer.Areas.Domain.JungleCamps.Models.MinorCamps
 {
     public abstract class MinorJungleCamp : JungleCamp
     {
-        protected MinorJungleCamp() : base(TimeSpan.FromMinutes(2))
+        protected MinorJungleCamp() : base(TimeSpan.FromSeconds(135))
         {
         }
     }
